ea1e4b193acdfd34e553947b05cb92b55cf5f0b0
JavierDominguezGomez <jdomgo3@gmail.com>
2019-10-29 20:01:09 +0100

    Creación de editor de texto.

---
+++ 15_src/editor.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import filedialog as FileDialog
+from io import open
+
+ruta = '' # Se utiliza para almacenar la ruta del fichero
+
+def nuevo():
+    global ruta
+    mensaje.set('Nuevo fichero')
+    ruta = ''
+    texto.delete(1.0, 'end')
+    root.title('Mi editor de texto')
+
+def abrir():
+    global ruta
+    mensaje.set('Abrir fichero')
+    ruta = FileDialog.askopenfilename(
+        title='Abrir fichero',
+        initialdir='.',
+        filetypes=(
+            ('Ficheros de texto', '*.txt'),
+            ('Todos los ficheros', '*.*')
+        )
+    )
+
+    if ruta != '':
+        fichero = open(ruta, 'r')
+        contenido = fichero.read()
+        texto.delete(1.0, 'end')
+        texto.insert('insert', contenido)
+        fichero.close()
+        root.title(ruta + ' - Mi editor de texto')
+
+def guardar():
+    mensaje.set('Guardar fichero')
+    if ruta != '':
+        contenido = texto.get(1.0, 'end')
+        fichero = open(ruta, 'w+')
+        fichero.write(contenido)
+        fichero.close()
+        mensaje.set('Fichero guardado con éxito')
+
+def guardar_como():
+    mensaje.set('Guardar fichero como')
+
+# Configuración de la raíz.
+root = Tk()
+root.title('Mi editor de texto')
+
+# Menú superior
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label='Nuevo', command=nuevo)
+filemenu.add_command(label='Abrir', command=abrir)
+filemenu.add_command(label='Guardar', command=guardar)
+filemenu.add_command(label='Guardar como', command=guardar_como)
+filemenu.add_separator()
+filemenu.add_command(label='Salir', command=root.quit)
+menubar.add_cascade(menu=filemenu, label='Archivo')
+
+# Caja de texto central
+texto = Text(root)
+texto.pack(fill='both', expand=1)
+texto.config(bd=0, padx=6, pady=4, font=('Courier New', 12))
+
+# Monitor inferior
+mensaje = StringVar()
+mensaje.set('Bienvenido a tu editor de texto')
+monitor = Label(root, textvar=mensaje, justify='left')
+monitor.pack(side='left')
+
+root.config(menu=menubar)
+root.mainloop()
--- 15_src/popups.py
+++ 15_src/popups.py
@@ -1,11 +1,36 @@
 from tkinter import *
 from tkinter import messagebox as MessageBox
+from tkinter import colorchooser as ColorChooser
+from tkinter import filedialog as FileDialog
 
 # Configuración de la raíz.
 root = Tk()
 
 def test():
-    MessageBox.showinfo('Hola!', 'Hola mundo!')
+    # MessageBox.showinfo('Hola!', 'Hola mundo!')
+    # MessageBox.showwarning('Alerta!', 'Sección solo para administradores.')
+    # MessageBox.showerror('Error!', 'Ha ocurrido un error inesperado.')
+    # resultado = MessageBox.askquestion('Salir', '¿Estás seguro que quieres salir sin guardar?')
+    # if resultado == 'yes':
+    #     root.destroy()
+    # resultado = MessageBox.askokcancel('Salir', '¿Sobreescribir el fichero actual?')
+    # resultado = MessageBox.askyesno('Salir', '¿Estás seguro que quieres salir sin guardar?')
+    # if resultado:
+    #     root.destroy()
+    # resultado = MessageBox.askretrycancel('Reintentar', 'No se puede conectar.')
+    # if resultado:
+    #     root.destroy()
+    # color = ColorChooser.askcolor(title='Elige un color.')
+    # print(color)
+    # ruta = FileDialog.askopenfilename(title='Abrir un fichero', initialdir='/',
+    #     filetypes=(('Ficheros de texto', '*.txt'),
+    #     ('Todos los ficheros', '*.*')))
+    # print(ruta)
+    # Equivale a open('path', 'w')
+    fichero = FileDialog.asksaveasfile(title='Guardar un fichero', mode='w', defaultextension='.txt')
+    if fichero is not None:
+        fichero.write('Hola!')
+        fichero.close()
 
 Button(root, text='Clícame', command=test).pack()
 
